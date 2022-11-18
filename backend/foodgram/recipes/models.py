6823db3475ee324edc0acdf98044e18a03ce49e8
Alex <alesha.zawadsky@yandex.ru>
2022-11-18 14:19:18 +0100

    add models to recipes

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        verbose_name='Название тега',
+    )
+    color = models.CharField(
+        max_length=7,
+        unique=True,
+        verbose_name='Цвет тега',
+    )
+    slug = models.SlugField(
+        unique=True,
+    )
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название ингредиента',
+    )
+    amount = models.PositiveSmallIntegerField()
+    measurement_unit = models.CharField(
+        max_length=50,
+    )
+
+
+class Recipe(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название рецепта',
+    )
+    image = models.ImageField(
+        upload_to='recipes',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+    )
+    cooking_time = models.PositiveSmallIntegerField()
+    description = models.TextField()
+    ingredient = models.ManyToManyField(
+        'Ingredient',
+        related_name='ingredients'
+    )
+    tag = models.ManyToManyField(
+        'Tag',
+        related_name='tags'
+    )
